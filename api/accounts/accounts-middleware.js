7a968703ccdc6731f8438aa3f39572ba2feabc83
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-11-01 17:03:14 -0400

    middleware near complete

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,36 @@
+const Account = require("./accounts-model");
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const { name, budget } = req.body;
+  if (!name || !budget) {
+    next({ status: 400, message: "name and budget are required" });
+  } else if (typeof name !== "string") {
+    next({ status: 400, message: "name of account must be a string" });
+  } else if (typeof budget !== "number") {
+    next({ status: 400, message: "budget of account must be a number" });
+  } else if (budget < 0 || budget > 1000000) {
+    next({
+      status: 400,
+      message: "budget of account is too large or too small",
+    });
+  } else {
+    next();
+  }
+};
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  Account.getById(req.body.id)
+    .then((account) => {
+      if (account) {
+        req.account = account;
+        next();
+      } else {
+        next({ status: 404, message: "account not found" });
+      }
+    })
+    .catch(next);
+};
